2f588892ff9295f52d6251f8fa5620700890a31e
Emmanuel Imhontu <imhontu.emmanuel@yahoo.com>
2021-12-06 14:03:38 +0100

    My portfolio website

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Start the project by typing `npm run dev` on the terminal press enter.
 In the terminal and navigate to the client folder.
 Install the project dependencies by typing `npm install` on the terminal.
 Start the project by typing `npm start` on the terminal.
+
+## My porfolio website
+
+https://emmanuel-1eea2.web.app
+
+Is going to be on https://emmanuelimhontu.se from 2pm 2021-12-07
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { useImages } from "../states/ImagesProvider";
 
 export default function Card({ item }) {
-//   const images = JSON.parse(sessionStorage.getItem("images"));
   const { images } = useImages();
   const { sku, name, price, color } = item;
 
   return (
     <div className="card">
-      {images && <img src={images[sku][0].src} alt="" />}
+      {images && <img src={images[sku][0]?.src} alt="" />}
 
       <div className="product-info">
         <h3>{name}</h3>
